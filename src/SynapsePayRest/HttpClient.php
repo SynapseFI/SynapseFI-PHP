0104c1c27108fc122c3bf42979bc723d22d0a35b
Nikki Galusha <nikki@Nikkis-MacBook-Pro.local>
2017-04-19 12:38:10 -0700

    Improved error handling

--- src/SynapsePayRest/HttpClient.php
+++ src/SynapsePayRest/HttpClient.php
@@ -3,7 +3,6 @@
 namespace SynapsePayRest;
 
 class HttpClient{
-	
 
 	function __construct($options, $user_id=null){
 		$this->client_id = $options['client_id'];
@@ -21,7 +20,7 @@ class HttpClient{
 	}
 
 	function update_headers($options){
-		if(array_key_exists('user_id',$options)){
+		if(array_key_exists('user_id', $options)){
 			$this->user_id = $options['user_id'];
 		}
 		if(array_key_exists('oauth_key', $options)){
@@ -60,24 +59,83 @@ class HttpClient{
 		return $options;
 	}
 
+	function handle_response($response,$ch){
+		$response = json_decode($response, true);
+		print_r($response);
+		if($response == false || curl_error($ch)) {
+			$err = curl_getinfo($ch);
+			curl_close($ch);
+			if($response != false){
+				return $response;
+			}
+			print $this->handle_errors($err);
+		} else {
+			curl_close($ch);
+			return $response;
+		}
+	}
+
+	function handle_errors($err){
+		print_r($err);
+
+		if($err['http_code'] == 0){
+			$err['http_code'] = 408;
+		}
+
+		switch ($err['http_code']){
+			case 205: $text = 'Reset Content'; break;
+			case 300: $text = 'Multiple Choices'; break;
+			case 301: $text = 'Moved Permanently'; break;
+			case 302: $text = 'Moved Temporarily'; break;
+			case 305: $text = 'Use Proxy'; break;
+			case 400: $text = 'Bad Request'; break;
+			case 401: $text = 'Unauthorized'; break;
+			case 402: $text = 'Payment Required'; break;
+			case 403: $text = 'Forbidden'; break;
+			case 404: $text = 'Not Found'; break;
+			case 405: $text = 'Method Not Allowed'; break;
+			case 406: $text = 'Not Acceptable'; break;
+			case 407: $text = 'Proxy Authentication Required'; break;
+			case 408: $text = 'Request timeout/Network connection error'; break;
+			case 409: $text = 'Conflict'; break;
+			case 410: $text = 'Gone'; break;
+			case 411: $text = 'Length Required'; break;
+			case 412: $text = 'Precondition Failed'; break;
+			case 413: $text = 'Payload Too Large'; break;
+			case 414: $text = 'Request-URI Too Large'; break;
+			case 415: $text = 'Unsupported Media Type'; break;
+			case 431: $text = 'Request header fields too large'; break;
+			case 500: $text = 'Internal Server Error'; break;
+			case 501: $text = 'Not Implemented'; break;
+			case 502: $text = 'Bad Gateway'; break;
+			case 503: $text = 'Service Unavailable'; break;
+			case 504: $text = 'Gateway Timeout'; break;
+			case 505: $text = 'HTTP Version not supported'; break;
+			case 521: $text = 'Web server is down'; break;
+			default:
+					$text = 'Unknown Error';
+			break;
+		}
+
+		return '{
+			"error":{
+				"en": '.$text.',
+			}
+			"error_code": "600",
+			"http_code": '.$err["http_code"].',
+			"success": false
+		}';
+	}
+
 	function get($path){
 		$url = $this->baseUrl . $path;
+		print $url;
 		$ch = curl_init();
 		curl_setopt($ch, CURLOPT_POST, false);
 		$options = $this->create_headers($url);
 		curl_setopt_array($ch, $options);
 		$response = curl_exec($ch);
-		if($response == false || curl_error($ch)) {
-			$err = curl_getinfo($ch);
-			curl_close($ch);
-			if($response != false){
-				return json_decode($response, true);
-			}
-			return $err;
-		} else {
-			curl_close($ch);
-			return json_decode($response, true);
-		}
+		return $this->handle_response($response,$ch);
 	}
 
 	function post($path, $json_object){
@@ -88,17 +146,7 @@ class HttpClient{
 		$options = $this->create_headers($url, $json_str);
 		curl_setopt_array($ch, $options);
 		$response = curl_exec($ch);
-		if($response == false || curl_error($ch)) {
-			$err = curl_getinfo($ch);                        
-			curl_close($ch);
-			if($response != false){
-				return json_decode($response, true);
-			}
-			return $err;
-		} else {
-			curl_close($ch);
-			return json_decode($response, true);
-		}
+		return $this->handle_response($response,$ch);
 	}
 
 	function patch($path, $json_object){
@@ -109,17 +157,7 @@ class HttpClient{
 		$options = $this->create_headers($url, $json_str);
 		curl_setopt_array($ch, $options);
 		$response = curl_exec($ch);
-		if($response == false || curl_error($ch)) {
-			$err = curl_getinfo($ch);                        
-			curl_close($ch);
-			if($response != false){
-				return json_decode($response, true);
-			}
-			return $err;
-		} else {
-			curl_close($ch);
-			return json_decode($response, true);
-		}
+		return $this->handle_response($response,$ch);
 	}
 
 	function delete($path){
@@ -129,16 +167,6 @@ class HttpClient{
 		$options = $this->create_headers($url);
 		curl_setopt_array($ch, $options);
 		$response = curl_exec($ch);
-		if($response == false || curl_error($ch)) {
-			$err = curl_getinfo($ch);                        
-			curl_close($ch);
-			if($response != false){
-				return json_decode($response, true);
-			}
-			return $err;
-		} else {
-			curl_close($ch);
-			return json_decode($response, true);
-		}
+		return $this->handle_response($response,$ch);
 	}
 }
